bf90a3cba0eaca24dbdcf08b9516223f677c0e37
jc321013 <jared.marcolongo@my.jcu.edu.au>
2016-08-29 09:20:36 +1000

    h

---
+++ Exercises/src/Alphabetize.java
@@ -0,0 +1,24 @@
+/**
+ * Created by jc321013 on 29/08/16.
+ */
+import java.util.*;
+
+public class Alphabetize
+{
+    public static void main(String[] args)
+    {
+        String StringOne, StringTwo, StringThree;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the 1st string: ");
+        StringOne = in.nextLine();
+        System.out.print("Enter the 2nd string: ");
+        StringTwo = in.nextLine();
+        System.out.print("Enter the 3rd string: ");
+        StringThree = in.nextLine();
+        if(StringOne.toLowerCase().compareTo(StringTwo.toLowerCase()) < 0 &&
+                StringTwo.toLowerCase().compareTo(StringThree.toLowerCase()) < 0)
+            System.out.println("Strings one, two and three that you entered are in alphabetical order");
+        else
+            System.out.println("Strings one, two and three that you entered are not in alphabetical order");
+    }
+}
